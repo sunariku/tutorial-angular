db160729e6e631457c1220e9e40da039e2397eaf
Made Subali <os.madesubali@gmail.com>
2025-12-05 11:14:17 +0800

    Penambahan validasi form create event.

--- src/app/pages/event/create/create.ts
+++ src/app/pages/event/create/create.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create.html',
   styleUrl: './create.css',
 })
 export class Create {
+  private formBuilder = inject(FormBuilder);
 
+  createEventForm = this.formBuilder.group({
+    title: this.formBuilder.control('', Validators.required),
+    picture: this.formBuilder.control('', Validators.required),
+    description: this.formBuilder.control('', Validators.required),
+  });
+
+  onSubmit(): void {
+    if (this.createEventForm.valid) {
+      const f = this.createEventForm.value;
+
+      const title = f.title;
+      const picture = f.picture;
+      const description = f.description;
+
+      alert(`Title: ${title}`);
+    }
+  }
 }
